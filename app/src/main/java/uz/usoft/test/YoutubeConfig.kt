c7dfcae0b46728286f2c3f4e2a56edcdf55d23a9
Sharapat Kalabaev <kalabaevshj@gmail.com>
2021-03-03 16:41:42 +0500

    removed YoutubeConfig. Not need

--- app/src/main/java/uz/usoft/test/YoutubeConfig.kt
+++
@@ -1,7 +0,0 @@
-package uz.usoft.test
-
-object YoutubeConfig {
-    const val API_KEY = "nFaV9DQn5o4lB0qGRYld2CbTFlLwLo/mjShQOmr+wz6nJkC9rLGP2V1StzmDqZ5vml7KtFONH/8"
-    const val ACCESS_TOKEN = "e7f97c3d-7839-4e78-bde1-e72112803874"
-
-}
